ce6e7ab1baba24c9b9c96a5d3e6f98503ad04960
Nalmohsen <nourulddin@gmail.com>
2024-01-24 21:53:53 -0800

    shopping

---
+++ express-shopping-list-solution/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+module.exports = app
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
